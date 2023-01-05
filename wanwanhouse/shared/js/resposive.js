769c8e37c696dc486bab530c50ef7838fbd7ede9
dauthingocthao12 <92421794+dauthingocthao12@users.noreply.github.com>
2023-01-05 13:35:36 +0900

    20220105

--- wanwanhouse/shared/js/resposive.js
+++ wanwanhouse/shared/js/resposive.js
@@ -15,41 +15,54 @@ $(document).ready(function () {
   });
 });
 
-jQuery(function ($) {
-      var first_width = $(window).width();
-
-      var timer = false;
-      $(window).resize(function () {
-        if (timer !== false) {
-          clearTimeout(timer);
-        }
-        timer = setTimeout(function () {
-          //resize完了時の動作
-          var width = $(window).width();
-          if (width != first_width) {
-
-            //console.log('resized');
-            fbiframe_reload();
-            first_width = width;
-          }
-        }, 200);
-      });
+  var first_width = $(window).width();
+  $(window).resize(function () {
+    if (first_width <= 500) {
+      console.log("AAAA");
+      var width="290";
+    }else{
+      var width="340";
+    }
+    var src = "https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FFacebook%2F&tabs=timeline&width="+width+"&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=false&appId"
+    $("#fbiframe").attr("src", "");
+    $("#fbiframe").attr("src", src);
+    $("#fbiframe").attr("width", width);
+  });
 
 
-      window.addEventListener('resize', function (event) {
-        if (window.innerWidth < 500) {
-        function fbiframe_reload() { //facebookウィジェットの再描画
-          var width = $(".facebook").width();
 
-          var src = "https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FFacebook%2F&tabs=timeline&width=" + width + "&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=false&appId"
-
-          $("#fbiframe").attr("src", "");
-          $("#fbiframe").attr("src", src);
-
-          $("#fbiframe").attr("width", width);
-        };
-
-          fbiframe_reload();
-          });
-        }
-      }, true);
+  // fbiframe_reload();
+//   jQuery(function($){
+//     var first_width = $(window).width();
+     
+//     var timer = false;
+//     $(window).resize(function() {
+//         if (timer !== false) {
+//             clearTimeout(timer);
+//         }
+//         timer = setTimeout(function() {
+//             //resize完了時の動作
+//             var width = $(window).width();
+//             if(width!=first_width){
+     
+//                 //console.log('resized');
+//                 fbiframe_reload();
+//                 first_width=width;
+//             }
+//         }, 200);
+//     });
+     
+//     function fbiframe_reload(){//facebookウィジェットの再描画
+     
+//             var width = $(".facebook").width();//親要素のID,class要変更
+     
+//             var src = "https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FFacebook%2F&tabs=timeline&width="+width+"&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=false&appId"
+     
+//             $("#fbiframe").attr("src","");       
+//             $("#fbiframe").attr("src",src); 
+     
+//             $("#fbiframe").attr("width",width); 
+//     };
+     
+//     fbiframe_reload();
+//     });
